fa695aa7abf58a9db7543dc56d98d797ba3e6705
danvarga1985 <danvarga1985@gmail.com>
2020-09-27 08:19:37 +0200

    Converting SpecialitySDJpaServiceTest to Behavior Driven style

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,109 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService service;
+
+    @Test
+    void testDeleteByObject() {
+        //given
+        Speciality speciality = new Speciality();
+
+        //when
+        service.delete(speciality);
+
+        //then
+        then(specialtyRepository).should().delete(any(Speciality.class));
+    }
+
+    @Test
+    void findByIdBddTest() {
+        //given
+        Speciality speciality = new Speciality();
+
+        given(specialtyRepository.findById(anyLong())).willReturn(Optional.of(speciality));
+
+        //when
+        Speciality foundSpeciality = service.findById(1L);
+
+        //then
+        assertThat(foundSpeciality).isNotNull();
+        then(specialtyRepository).should(times(1)).findById(anyLong());
+        then(specialtyRepository).shouldHaveNoMoreInteractions();
+    }
+
+    @Test
+    void findById() {
+        Speciality speciality = new Speciality();
+
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+
+        assertThat(foundSpeciality).isNotNull();
+
+        verify(specialtyRepository).findById(anyLong());
+    }
+
+    @Test
+    void testDeleteById() {
+        //given - none
+
+        //when
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        //then
+        then(specialtyRepository).should(times(2)).deleteById(1L);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(anyLong());
+    }
+
+    @Test
+    void deleteByAtMost() {
+
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, atMost(5)).deleteById(anyLong());
+    }
+
+    @Test
+    void deleteByIdNever() {
+        service.deleteById(1L);
+        service.deleteById(1L);
+
+        verify(specialtyRepository, never()).delete(any(Speciality.class));
+
+    }
+
+    @Test
+    void testDelete() {
+        service.delete(new Speciality());
+    }
+}
